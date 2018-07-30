c40c8042251548dabcf8657d730ba49f3c4619ae
surenderthakran <surender.thakran89@gmail.com>
2018-07-30 15:55:33 +0530

    Updated test cases for activation.go

--- activation/activation_test.go
+++ activation/activation_test.go
@@ -10,6 +10,34 @@ func TestSupportedActivationFunctions(t *testing.T) {
 	result := SupportedActivationFunctions()
 
 	if !cmp.Equal(activationFunctions, result) {
-		t.Errorf("SupportedActivationFunctions() = %v, want %v", activationFunctions, result)
+		t.Errorf("SupportedActivationFunctions() = %v, want %v", result, activationFunctions)
+	}
+}
+
+func TestValidFunction(t *testing.T) {
+	testCases := []struct {
+		input string
+		want  string
+	}{
+		{
+			input: "sigmoid",
+			want:  "SIGMOID",
+		},
+		{
+			input: "identity",
+			want:  "",
+		},
+		{
+			input: "leaky relu",
+			want:  "",
+		},
+	}
+
+	for _, test := range testCases {
+		result := ValidFunction(test.input)
+
+		if result != test.want {
+			t.Errorf("ValidFunction(%s) = %s, want %s", test.input, result, test.want)
+		}
 	}
 }
