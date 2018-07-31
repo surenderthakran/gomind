3fbec64361e56ebffd1ac6fa62cfc4784c968736
surenderthakran <surender.thakran89@gmail.com>
2018-07-31 11:06:36 +0530

    Updated activation_test.go

--- activation/activation_test.go
+++ activation/activation_test.go
@@ -1,10 +1,49 @@
 package activation
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 )
+
+func TestNew(t *testing.T) {
+	testCases := []struct {
+		name    string
+		service *Service
+		err     error
+	}{
+		{
+			name: "identity",
+			err:  fmt.Errorf("invalid activation function: . Activation Functions should be amongst: %v", activationFunctions),
+		},
+		{
+			name: "sigmoid",
+			service: &Service{
+				name: "SIGMOID",
+			},
+		},
+	}
+
+	for _, test := range testCases {
+		service, err := New(test.name)
+		if err != nil && err.Error() != test.err.Error() {
+			t.Errorf("New(%s) = _, %v, want _, %v", test.name, err, test.err)
+		} else if !cmp.Equal(test.service, service, cmp.AllowUnexported(Service{})) {
+			t.Errorf("New(%s) = %v, nil, want %v, nil", test.name, service, test.service)
+		}
+	}
+}
+
+func TestName(t *testing.T) {
+	service, _ := New("sigmoid")
+
+	name := service.Name()
+
+	if name != "SIGMOID" {
+		t.Errorf("Service.Name() = %v, want %v", name, "SIGMOID")
+	}
+}
 
 func TestSupportedActivationFunctions(t *testing.T) {
 	result := SupportedActivationFunctions()
